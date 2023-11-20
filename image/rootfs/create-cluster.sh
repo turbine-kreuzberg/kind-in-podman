21f59e984deb3020811e04c4b98c612225baf619
Adam Hukalowicz <procing3r@gmail.com>
2023-11-20 06:57:51 +0100

    init

---
+++ image/rootfs/create-cluster.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+kubelet_logs () {
+  #KIND_CONTAINER=$(podman container ls --format "{{.ID}}")
+  #echo $KIND_CONTAINER
+  #[[ ! -z "$KIND_CONTAINER" ]] && podman container exec -ti ${KIND_CONTAINER} journalctl -fu kubelet.service
+
+  while [[ -z $KIND_CONTAINER ]];
+  do
+    KIND_CONTAINER=$(podman container ls --format "{{.ID}}")
+    sleep 5;
+  done
+
+  podman container exec -ti ${KIND_CONTAINER} journalctl -fu kubelet.service
+}
+
+/usr/bin/kind create cluster --config /kind-in-podman.yaml -v 8 &
+
+kubelet_logs
